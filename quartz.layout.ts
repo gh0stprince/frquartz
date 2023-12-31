10e56a89edb3e502899fbd94d5c5db8d7e185fa9
Meloran <103946889+gh0stprince@users.noreply.github.com>
2023-12-31 12:24:57 -0500

    Quartz sync: Dec 31, 2023, 12:24 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -16,17 +16,19 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    
+    Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
-
+    Component.TagList(),
   ],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
   ],
   right: [
-    
+ 
+    Component.DesktopOnly(Component.TableOfContents()),
+
   ],
 }
 
