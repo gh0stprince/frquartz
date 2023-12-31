8f81295ec91122622b1e8a1f14b2d2dd57a013b0
Meloran <103946889+gh0stprince@users.noreply.github.com>
2023-12-31 12:12:53 -0500

    Quartz sync: Dec 31, 2023, 12:12 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "no rest for the wicked",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "gh0stprince.github.io/frquartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
@@ -19,25 +19,25 @@ const config: QuartzConfig = {
         code: "IBM Plex Mono",
       },
       colors: {
-        lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-        },
-        darkMode: {
-          light: "#161618",
-          lightgray: "#393639",
-          gray: "#646464",
-          darkgray: "#d4d4d4",
-          dark: "#ebebec",
-          secondary: "#7b97aa",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
+  lightMode: {
+    light: "#fbf1c7",
+    lightgray: "#f9f5d7",
+    gray: "#665c54",
+    darkgray: "#282828",
+    dark: "#9d0006",
+    secondary: "#af3a03",
+    tertiary: "#458588",
+    highlight: "rgba(189, 174, 147, 0.15)",
+  },
+  darkMode: {
+    light: "#282828",
+    lightgray: "#1d2021",
+    gray: "#bdae93",
+    darkgray: "#fbf1c7",
+    dark: "#cc241d",
+    secondary: "#d65d0e",
+    tertiary: "#d79921",
+    highlight: "rgba(168, 153, 132, 0.15);",
         },
       },
     },
@@ -64,7 +64,7 @@ const config: QuartzConfig = {
       Plugin.FolderPage(),
       Plugin.TagPage(),
       Plugin.ContentIndex({
-        enableSiteMap: true,
+        enableSiteMap: false,
         enableRSS: true,
       }),
       Plugin.Assets(),
